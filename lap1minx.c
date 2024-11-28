4c1b8093b34e699723a6e8208a8a80f5c28d1dc1
6704062610224 <s6704062610224@email.kmutnb.ac.th>
2024-11-28 15:02:18 +0700

    ข้อ2

--- lap1minx.c
+++ lap1minx.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 int main(){
-	int num1=5;
-	int num2=10;
-	if (num1>num2){
-		printf("num1 greater than num2");
+	int num1;
+	int num2;
+	int sum;
+	scanf("%d",&num1);
+	scanf("%d",&num2);
+	if(num1>num2){
+		sum=num1-num2;
+		printf("num1 greater than num2 \n");
+		printf("%d",sum);
 	}else{
-		printf("num2 greater than num1");
+		sum=num2-num1;
+		printf("num2 greater than num1 \n");
+		printf("%d",sum);
 	}
 }
